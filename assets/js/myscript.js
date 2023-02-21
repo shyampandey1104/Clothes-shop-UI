182aa99f7c2a05120ae733e2bc4f7c2723663617
shyampandey1104 <95642167+shyampandey1104@users.noreply.github.com>
2023-02-21 21:05:50 +0530

    Changes

---
+++ assets/js/myscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.btn-show').click( function(){
+        $(".filter-column").show('slow');
+    });
+
+    $('.btn-close').click( function(){
+        $(".filter-column").hide('slow');
+    });
+    
+  
+});
